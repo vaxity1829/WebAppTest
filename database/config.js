301deae3ace30462d70352d01c03fa7a856f7650
vaxity1829 <vaxity1829@outlook.com>
2026-06-20 21:53:23 +0700

    Update config.js

--- database/config.js
+++ database/config.js
@@ -4,7 +4,7 @@
 // it still works, but nothing is saved permanently or shared between visitors.
 
 window.APP_CONFIG = {
-  GOOGLE_CLIENT_ID: "YOUR_CLIENT_ID.apps.googleusercontent.com",
-  SHEETS_API_URL: "",
+  GOOGLE_CLIENT_ID: "621125969151-afd2cskmdumktc9lo91trjq8b10dmr9t.apps.googleusercontent.com",
+  SHEETS_API_URL: "https://script.google.com/macros/library/d/1k3GTuhgpUsOYw_4W9GQwnPQDu_NTbWBFjtSiNldbVVRaOjMlTca6qnU9/1",
   APP_NAME: "ห้องพักครู",
 };
